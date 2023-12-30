6a2c6baf5d0807bb4d6c7829113a7036f5eb7dc0
Bruno Azzi <bruno.azzi@icloud.com>
2023-12-30 10:18:06 -0300

    feat: add gallery

---
+++ app/components/ImageGallery.tsx
@@ -0,0 +1,5 @@
+export const DudasGallery = () => (
+  <div className="my-16">
+    <img src="images/gallery.png" className="w-full" alt="gallery" />
+  </div>
+);
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,40 +4,48 @@ import { CaseCard, VerticalCaseCard } from "~/components/Card";
 import { ContactSection } from "~/components/ContactSetup";
 import { Footer } from "~/components/Footer";
 import { Header } from "~/components/Header";
+import { DudasGallery } from "~/components/ImageGallery";
 
 export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
 
 export default function Index() {
   return (
-    <div className="flex flex-col gap-14 mx-auto px-20">
-      <Header />
-      <div className="grid grid-cols-2 gap-12">
-        <VerticalCaseCard
-          title="Telemedicina"
-          description="Uma solução para profissionais da saúde pública garantirem que os protocolos de saúde sejam respeitados na infância e adolescência."
-          link="/telemedicina"
-          tags={["Desktop"]}
-        >
-          <img src="images/telemedicina-mockup.png" />
-        </VerticalCaseCard>
-        <VerticalCaseCard
-          title="E-learning"
-          description="Uma solução para profissionais da saúde pública garantirem que os protocolos de saúde sejam respeitados na infância e adolescência."
-          link="/e-learning"
-          tags={["Desktop", "Responsivo"]}
-        >
-          <img src="images/restaurante.png" />
-        </VerticalCaseCard>
-        <CaseCard
-          title="Logística"
-          description="Uma solução para profissionais da saúde pública garantirem que os protocolos de saúde sejam respeitados na infância e adolescência."
-          link="/logistica"
-          className="col-span-full"
-          tags={["Desktop"]}
-        />
+    <div className="flex flex-col">
+      <div className="flex flex-col px-20 gap-14">
+        <Header />
+        <div className="grid grid-cols-2 gap-12">
+          <VerticalCaseCard
+            title="Telemedicina"
+            description="Uma solução para profissionais da saúde pública garantirem que os protocolos de saúde sejam respeitados na infância e adolescência."
+            link="/telemedicina"
+            tags={["Desktop"]}
+          >
+            <img src="images/telemedicina-mockup.png" />
+          </VerticalCaseCard>
+          <VerticalCaseCard
+            title="E-learning"
+            description="Uma solução para profissionais da saúde pública garantirem que os protocolos de saúde sejam respeitados na infância e adolescência."
+            link="/e-learning"
+            tags={["Desktop", "Responsivo"]}
+          >
+            <img src="images/restaurante.png" />
+          </VerticalCaseCard>
+          <CaseCard
+            title="Logística"
+            description="Uma solução para profissionais da saúde pública garantirem que os protocolos de saúde sejam respeitados na infância e adolescência."
+            link="/logistica"
+            className="col-span-full"
+            tags={["Desktop"]}
+          />
+        </div>
+        <ContactSection />
       </div>
-      <ContactSection />
-      <Footer />
+
+      <DudasGallery />
+
+      <div className="flex flex-col px-20 gap-14">
+        <Footer />
+      </div>
     </div>
   );
 }
